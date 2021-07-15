1a9a84b32fd44c46ed010412ed09c5e54a09816d
TimonPeng <timonminipeng@gmail.com>
2021-07-15 20:18:25 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>electron-react-boilerplate</h1>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
